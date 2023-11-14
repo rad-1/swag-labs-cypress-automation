9698c6d5e30eb47ee51ff9165b84f442a59fd838
Joshua Tipton <jctipton123@gmail.com>
2023-11-14 14:59:21 -0600

    Format constants

--- cypress/support/utils/login.utils.js
+++ cypress/support/utils/login.utils.js
@@ -7,14 +7,14 @@ const SELECTORS = {
     LOGIN_ERROR: 'error'
 };
 
-export const CREDENTIALS = {
+const CREDENTIALS = {
     VALID_USERNAME: Cypress.env('username'),
     VALID_PASSWORD: Cypress.env('password'),
     INVALID_USERNAME: 'invalid_username',
     INVALID_PASSWORD: 'invalid_password'
 };
 
-export const LOGIN_ERRORS = {
+const LOGIN_ERRORS = {
     NO_MATCH: 'Epic sadface: Username and password do not match any user in this service',
     USERNAME_REQUIRED: 'Epic sadface: Username is required',
     PASSWORD_REQUIRED: 'Epic sadface: Password is required'
@@ -24,3 +24,4 @@ export const usernameField = () => cy.getBySel(SELECTORS.USERNAME_FIELD);
 export const passwordField = () => cy.getBySel(SELECTORS.PASSWORD_FIELD);
 export const loginButton = () => cy.getBySel(SELECTORS.LOGIN_BUTTON);
 export const loginError = () => cy.getBySel(SELECTORS.LOGIN_ERROR);
+export { CREDENTIALS, LOGIN_ERRORS };

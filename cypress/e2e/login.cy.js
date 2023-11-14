f53431e4ae4581849afc3c5b834bb6996354f099
Joshua Tipton <jctipton123@gmail.com>
2023-11-14 15:55:29 -0600

    Update tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -53,38 +53,44 @@ describe('login page', { tags: ['@login', '@smoke'] }, () => {
                 usernameField().type(CREDENTIALS.INVALID_USERNAME);
                 passwordField().type(CREDENTIALS.VALID_PASSWORD);
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.NO_MATCH);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.NO_MATCH);
             });
 
             it('invalid password', () => {
                 usernameField().type(CREDENTIALS.VALID_USERNAME);
                 passwordField().type(CREDENTIALS.INVALID_PASSWORD);
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.NO_MATCH);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.NO_MATCH);
             });
 
             it('invalid username and password', () => {
                 usernameField().type(CREDENTIALS.INVALID_USERNAME);
                 passwordField().type(CREDENTIALS.INVALID_PASSWORD);
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.NO_MATCH);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.NO_MATCH);
             });
 
             it('empty username field', () => {
                 passwordField().type(CREDENTIALS.VALID_PASSWORD);
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.USERNAME_REQUIRED);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.USERNAME_REQUIRED);
             });
 
             it('empty password field', () => {
                 usernameField().type(CREDENTIALS.VALID_USERNAME);
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.PASSWORD_REQUIRED);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.PASSWORD_REQUIRED);
             });
 
             it('empty username and password fields', () => {
                 loginButton().click();
-                loginError().should('have.text', LOGIN_ERRORS.USERNAME_REQUIRED);
+
+                loginError().should('be.visible').and('have.text', LOGIN_ERRORS.USERNAME_REQUIRED);
             });
         });
     });

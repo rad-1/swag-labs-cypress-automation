a30ef9273d538ef33cfb4a651081536c426211f3
Joshua Tipton <jctipton123@gmail.com>
2023-11-15 01:11:36 -0600

    Add tests

--- cypress/e2e/inventory.cy.js
+++ cypress/e2e/inventory.cy.js
@@ -1,5 +1,7 @@
 // cypress/e2e/inventory.cy.js
 
+import { PRODUCTS } from '../support/constants/inventory.constants';
+
 describe('inventory page', { tags: ['@inventory', '@smoke'] }, () => {
     beforeEach(() => {
         cy.loginViaUi();
@@ -10,8 +12,23 @@ describe('inventory page', { tags: ['@inventory', '@smoke'] }, () => {
             cy.get('span.title').should('be.visible').and('have.text', 'Products');
         });
 
-        it('products are visible', () => {
+        it('six products are visible', () => {
             cy.get('div.inventory_item').should('be.visible').and('have.length', 6);
+        });
+
+        it('product names and prices are visible', () => {
+            PRODUCTS.forEach((product, index) => {
+                cy.get('div.inventory_item')
+                    .eq(index)
+                    .within(() => {
+                        cy.get('div.inventory_item_name')
+                            .should('be.visible')
+                            .and('have.text', product.NAME);
+                        cy.get('div.inventory_item_price')
+                            .should('be.visible')
+                            .and('have.text', product.PRICE);
+                    });
+            });
         });
     });
 });
---
+++ cypress/support/constants/inventory.constants.js
@@ -0,0 +1,28 @@
+const PRODUCTS = [
+    {
+        NAME: 'Sauce Labs Backpack',
+        PRICE: '$29.99',
+    },
+    {
+        NAME: 'Sauce Labs Bike Light',
+        PRICE: '$9.99',
+    },
+    {
+        NAME: 'Sauce Labs Bolt T-Shirt',
+        PRICE: '$15.99'
+    },
+    {
+        NAME: 'Sauce Labs Fleece Jacket',
+        PRICE: '$49.99'
+    },
+    {
+        NAME: 'Sauce Labs Onesie',
+        PRICE: '$7.99'
+    },
+    {
+        NAME: 'Test.allTheThings() T-Shirt (Red)',
+        PRICE: '$15.99'
+    },
+];
+
+export { PRODUCTS };

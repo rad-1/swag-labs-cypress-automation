b778850a299b30a9dba532381cb86af0ebc103cb
Joshua Tipton <jctipton123@gmail.com>
2023-11-16 15:07:14 -0600

    Update loginViaUi() command

--- cypress/support/commands/common.commands.js
+++ cypress/support/commands/common.commands.js
@@ -4,13 +4,18 @@ Cypress.Commands.add('getBySel', (selector) => {
     return cy.get(`[data-test=${selector}]`)
 })
 
-Cypress.Commands.add('loginViaUi', () => {
+Cypress.Commands.add('loginViaUi', (username, password) => {
     cy.visit('/', {
         // https://github.com/cypress-io/cypress/issues/27501
         onBeforeLoad(win) {
             delete win.navigator.__proto__.serviceWorker
         }
     })
-    cy.login(Cypress.env('username'), Cypress.env('password'))
+
+    cy.login(
+        username || Cypress.env('username'),
+        password || Cypress.env('password')
+    )
+
     cy.url().should('contain', '/inventory.html')
 })

b60a0d009bd6f15a0e5064bae85a9969843e8e71
Joshua Tipton <jctipton123@gmail.com>
2023-11-17 21:28:37 -0600

    Linting

--- cypress/support/commands/login.commands.js
+++ cypress/support/commands/login.commands.js
@@ -1,7 +1,7 @@
 // cypress/support/login.commands.js
 
 Cypress.Commands.add('login', (username, password) => {
-    cy.getBySel('username').type(username);
-    cy.getBySel('password').type(password);
-    cy.getBySel('login-button').click();
+    cy.getBySel('username').type(username)
+    cy.getBySel('password').type(password)
+    cy.getBySel('login-button').click()
 });

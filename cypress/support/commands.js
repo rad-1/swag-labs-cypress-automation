53fc0aa0d66f65795aee63a251581057d2b6b442
Joshua Tipton <jctipton123@gmail.com>
2023-11-28 04:56:44 -0600

    Formatting

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,11 +3,11 @@
 import { LOGIN_PAGE, INVENTORY_PAGE } from './constants'
 
 Cypress.Commands.add('getBySel', (selector) => {
-    return cy.get(`[data-test=${selector}]`)
+    cy.get(`[data-test=${selector}]`)
 })
 
 Cypress.Commands.add('getBySelLike', (selector) => {
-    return cy.get(`[data-test*=${selector}]`)
+    cy.get(`[data-test*=${selector}]`)
 })
 
 Cypress.Commands.add('submitLoginForm', (username, password) => {
